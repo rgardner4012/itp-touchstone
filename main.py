48285fa1b2ca05ccb4ea20757638385614cedeea
Rick Sanchez <36778339-fruitfocker@users.noreply.replit.com>
2024-09-26 13:06:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from bs4 import BeautifulSoup as bs
+import csv
+
+# Chrome driver path in replit?
+#chrome_driver_path = ""
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver.maximize_window()
+
+# URL of the website to scrape
+url = "https://partners.wgu.edu/transfer-pathway-agreement?uniqueId=BSCS7110&collegeCode=IT&instId=796&programId=37"
+
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
+
+options.add_argument("start-maximized"); 
+options.add_argument("disable-infobars"); 
+options.add_argument("--disable-extensions"); 
+options.add_argument("--disable-gpu"); 
+options.add_argument("--disable-dev-shm-usage"); 
+
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(5)
+
+driver.get(url)
+table_body = driver.find_element(By.XPATH, '/html/body/app-root/div/builder-component[2]/span/span/builder-content/builder-blocks/div/div/builder-component-element/div/div/div/div/div/div/div[3]/div/div/div[2]/div/div/div/div/div/div/div[2]/wgu-tpa-table/table[1]/tbody')
+table_rows = table_body.find_elements(By.TAG_NAME, "tr")
+with open("output.csv", "w") as file:
+  writer = csv.writer(file)
+  
+  for row in table_rows:
+    table_data = row.find_elements(By.TAG_NAME, "td")
+    row_data =  []
+    for data in table_data:
+      row_data.append(data.text)
+    writer.writerow(row_data)
+    
+driver.quit()
+
+
+# # Check if the request was successful
+# if session.status_code == 200:
+#     # Parse the HTML content using BeautifulSoup
+#     soup = BeautifulSoup(r.content, "html.parser")
+#     print(soup)
+#     # Find the relevant section of the page. For now, just print
+#     # the body of the page
+#     body_content = soup.find("body")
+#     if body_content:
+#         # print relevant content
+#         print("****BODY****")
+#         print(body_content.text)
+#     else:
+#         print("Couldn't find the body content.")
+# else:
+#     print(f"Failed to retrieve the page. Status code: {response.status_code}")
---
+++ replit.nix
@@ -0,0 +1,11 @@
+{pkgs}: {
+  deps = [
+    pkgs.vim
+    pkgs.zlib
+    pkgs.xcodebuild
+    pkgs.geckodriver
+    pkgs.chromium
+    pkgs.chromedriver
+    pkgs.ungoogled-chromium
+  ];
+}
